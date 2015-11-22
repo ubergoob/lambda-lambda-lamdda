6d22b8c9243a84682325d67e39867676df9f3d2b
Paul Bierly <ubergoober@gmail.com>
2015-11-21 22:18:22 -0600

    made the return object the event object

--- lambda_template/index.js
+++ lambda_template/index.js
@@ -1,4 +1,5 @@
 exports.handler = function (event, context) {
   // put your code in here...
-  context.done(null, "webpage object from the provided event.json file: " + event.webpage);
+  //context.done(null, "webpage object from the provided event.json file: " + event.webpage);
+  context.done(null, JSON.stringify(event));
 };
